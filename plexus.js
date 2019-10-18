d72254246418c1d2423863f5b40215fbee8c105b
Burak <burak.kelleroglu@ug.bilkent.edu.tr>
2019-10-18 21:43:16 +0300

    Initial Commit
    implement docs of PlexusAPI

---
+++ plexus.js
@@ -0,0 +1,21 @@
+mermaid.initialize({
+        theme: 'neutral',
+        startOnLoad: false,
+        sequence: {
+            mirrorActors: false,
+            bottomMarginAdj: 5,
+            height: 50
+        }
+    });
+var specUrl = 'openapi.yaml'
+Redoc.init(
+    specUrl,
+    {},
+    document.getElementsByTagName('body')[0],
+    function () {
+        window.setTimeout(function (){
+            mermaid.init('.diagram');
+        }, 500);
+    }
+);
+
